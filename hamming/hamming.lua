124fb51650a1b1526ae3a103342be064e1873d69
Adam Hall <adamhalldesigns@gmail.com>
2021-11-01 07:57:44 +0000

    a few done

---
+++ hamming/hamming.lua
@@ -0,0 +1,15 @@
+local Hamming = {}
+
+function Hamming.compute(a, b)
+  if a:len() ~= b:len() then return -1 end
+
+  local diff = 0
+
+  for i = 1, #a do if a:sub(i, i) ~= b:sub(i, i) then diff = diff + 1 end end
+
+  return diff
+
+end
+
+return Hamming
+
