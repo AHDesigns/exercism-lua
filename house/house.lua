6afc8b185a44758689ca4690fa561828d01bbef3
Adam Hall <adamhalldesigns@gmail.com>
2021-11-02 07:26:22 +0000

    found this annoyingly hard

--- house/house.lua
+++ house/house.lua
@@ -1,6 +1,30 @@
 local house = {}
 
 house.verse = function(which)
+  -- bored now
+  --
+  local objects = {
+    {object = 'house ', action = 'Jack built.'},
+    {object = 'malt\n', action = 'lay in'}, {object = 'rat\n', action = 'ate'},
+    {action = 'worried', object = 'cat'}, {action = 'tossed', object = 'dog'},
+    {action = 'milked', object = 'cow with the crumpled horn'},
+    {action = 'kissed', object = 'maiden all forlorn'},
+    {action = 'married', object = 'man all tattered and torn'},
+    {action = 'woke', object = 'priest all shaven and shorn'},
+    {action = 'kept', object = 'rooster that crowed in the morn'},
+    {action = 'belonged to', object = 'farmer sowing his corn'},
+    {action = 'horse', object = 'horse and the hound and the horn'}
+
+  }
+
+  local firstLine = 'This is'
+
+  for verse = which, 1, -1 do
+    firstLine = firstLine .. ' the ' .. objects[verse].object .. 'that '
+                    .. objects[verse].action
+  end
+
+  return firstLine
 
 end
 
--- house/house_spec.lua
+++ house/house_spec.lua
@@ -1,17 +1,13 @@
 local house = require('house')
 
 local verses = {
-  [[This is the house that Jack built.]],
-  [[This is the malt
-that lay in the house that Jack built.]],
-  [[This is the rat
+  [[This is the house that Jack built.]], [[This is the malt
+that lay in the house that Jack built.]], [[This is the rat
 that ate the malt
-that lay in the house that Jack built.]],
-  [[This is the cat
+that lay in the house that Jack built.]], [[This is the cat
 that killed the rat
 that ate the malt
-that lay in the house that Jack built.]],
-  [[This is the dog
+that lay in the house that Jack built.]], [[This is the dog
 that worried the cat
 that killed the rat
 that ate the malt
@@ -21,8 +17,7 @@ that tossed the dog
 that worried the cat
 that killed the rat
 that ate the malt
-that lay in the house that Jack built.]],
-  [[This is the maiden all forlorn
+that lay in the house that Jack built.]], [[This is the maiden all forlorn
 that milked the cow with the crumpled horn
 that tossed the dog
 that worried the cat
@@ -55,8 +50,7 @@ that tossed the dog
 that worried the cat
 that killed the rat
 that ate the malt
-that lay in the house that Jack built.]],
-  [[This is the farmer sowing his corn
+that lay in the house that Jack built.]], [[This is the farmer sowing his corn
 that kept the rooster that crowed in the morn
 that woke the priest all shaven and shorn
 that married the man all tattered and torn
@@ -82,12 +76,13 @@ that lay in the house that Jack built.]]
 }
 
 describe('house', function()
-  it('should correctly generate the first verse', function()
+  it('#only should correctly generate the first verse', function()
     assert.equal(verses[1], house.verse(1))
   end)
 
   it('should correctly generate all verses', function()
     for i = 1, #verses do
+      print(i, 'start', house.verse(i), 'end')
       assert.equal(verses[i], house.verse(i))
     end
   end)
